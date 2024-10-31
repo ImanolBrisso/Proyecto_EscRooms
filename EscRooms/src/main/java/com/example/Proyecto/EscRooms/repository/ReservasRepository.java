bcf2ee38acbe2a93443d5459635769810d8a2df8
ImanolBrisso <imanolbrissolese22@gmail.com>
2024-10-30 22:04:54 -0300

    Se modifica uso y confirmaciones de repositorys

--- EscRooms/src/main/java/com/example/Proyecto/EscRooms/repository/ReservasRepository.java
+++ EscRooms/src/main/java/com/example/Proyecto/EscRooms/repository/ReservasRepository.java
@@ -1,4 +1,27 @@
 package com.example.Proyecto.EscRooms.repository;
 
-public interface ReservasRepository {
+import com.example.Proyecto.EscRooms.Modelo.Reservas;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface ReservasRepository extends JpaRepository<Reservas, Long> {
+
+    // Búsqueda de reservas por fecha
+    List<Reservas> findByFechaReservaBetween(LocalDateTime inicio, LocalDateTime fin);
+
+    // Búsqueda de reservas por Id de sala
+    List<Reservas> findBySalaEscapeId(Long salaId);
+
+    // Búsqueda de reservas por cliente o usuario
+    List<Reservas> findByClienteEmail(String email);
+
+    // Búsqueda y confirmación de reserva por horarios y fecha
+    boolean existsByFechaReservaAndSalaEscapeId(LocalDateTime fechaReserva, Long salaId);
+
+    // Confirmación y conteo de reservas por sala para fecha especifica
+    Long countBySalaEscapeId(LocalDateTime salaId);
 }
--- EscRooms/src/main/java/com/example/Proyecto/EscRooms/repository/SalaEscapeRepository.java
+++ EscRooms/src/main/java/com/example/Proyecto/EscRooms/repository/SalaEscapeRepository.java
@@ -1,4 +1,28 @@
 package com.example.Proyecto.EscRooms.repository;
 
-public interface SalaEscapeRepository {
+import com.example.Proyecto.EscRooms.Modelo.SalaEscape;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SalaEscapeRepository extends JpaRepository<SalaEscape, Long> {
+
+    // Busqueda de sala por dificultad
+    List<SalaEscape> findByDificultad(String dificultad);
+
+    // Busqueda de sala por capacidad ya sea minima o máxima
+    List<SalaEscape> findByCapacidadMinLessThanEqualAndCapacidadMaxGreaterThanEqual(int personas, int personas2);
+
+    // Búsqueda de sala por nombre
+    Optional<SalaEscape> findByNombreContainingIgnoreCase(String nombre);
+
+    // Búsqueda de salas disponibles
+    List<SalaEscape> findByActivoTrue();
+
+    // Búsqueda de salas por precio
+    List<SalaEscape> findByPrecioBetween(double precioMin, double precioMax);
+
 }
