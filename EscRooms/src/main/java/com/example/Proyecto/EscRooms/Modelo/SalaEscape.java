9005aa5d520318befe4c15661fb840791bb7d661
ImanolBrisso <imanolbrissolese22@gmail.com>
2024-11-03 22:09:17 -0300

    getters y setters

--- EscRooms/src/main/java/com/example/Proyecto/EscRooms/Modelo/SalaEscape.java
+++ EscRooms/src/main/java/com/example/Proyecto/EscRooms/Modelo/SalaEscape.java
@@ -35,10 +35,94 @@ public class SalaEscape {
     @Column(nullable = false)
     private boolean activo;
 
-    public void setId(Long id) {
+    // Constructor
+    public SalaEscape(String nombre, String descripcion, String dificultad, int capacidadMin, int capacidadMax,
+                      BigDecimal precio, int duracionMinutos, boolean activo) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.dificultad = dificultad;
+        this.capacidadMin = capacidadMin;
+        this.capacidadMax = capacidadMax;
+        this.precio = precio;
+        this.duracionMinutos = duracionMinutos;
+        this.activo = activo;
     }
 
-    // Creados los siguientes parametros getters y setters.
+    public SalaEscape() {
+    }
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDificultad() {
+        return dificultad;
+    }
+
+    public void setDificultad(String dificultad) {
+        this.dificultad = dificultad;
+    }
+
+    public int getCapacidadMin() {
+        return capacidadMin;
+    }
+
+    public void setCapacidadMin(int capacidadMin) {
+        this.capacidadMin = capacidadMin;
+    }
+
+    public int getCapacidadMax() {
+        return capacidadMax;
+    }
+
+    public void setCapacidadMax(int capacidadMax) {
+        this.capacidadMax = capacidadMax;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public int getDuracionMinutos() {
+        return duracionMinutos;
+    }
+
+    public void setDuracionMinutos(int duracionMinutos) {
+        this.duracionMinutos = duracionMinutos;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
 }
 
 
